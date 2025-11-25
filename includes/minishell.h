4dbb60cf3c32a65f52935e951bf55363d95ad882
Giti Khavari <gkhavari@c3r8p5.42vienna.com>
2025-11-25 21:57:57 +0100

    included a shell struct for env variables

--- includes/minishell.h
+++ includes/minishell.h
@@ -18,4 +18,12 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
+typedef struct s_shell
+{
+    char    **envp;        // copy of environment variables
+    char    *user;         // current username
+    char    cwd[256];      // current working directory
+    int     last_exit;     // last command exit status
+}   t_shell;
+
 #endif
