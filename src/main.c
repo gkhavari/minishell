e756ce52d3d5be1d07643299bf9951b71869058f
Giti Khavari <gkhavari@c3r8p5.42vienna.com>
2025-11-25 21:57:09 +0100

    build prompt function included

--- src/main.c
+++ src/main.c
@@ -12,16 +12,25 @@
 
 #include "../includes/minishell.h"
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
+    t_shell	shell;
 	char	*input;
+	char	*promt;
+	(void) argc;
+	(void) argv;
 
+	init_shell(&shell, envp);
 	while (1)
 	{
-		input = readline("minishell$ ");
+		promt = build_prompt(&shell);
+		if (!promt)
+			continue;
+        input = readline(promt);
+		free(promt);
 		if (!input)
 		{
-			printf("exit\n");
+			perror("Error: readline\n");
 			break ;
 		}
 		if (*input) /*don't add empty prompts to history*/
