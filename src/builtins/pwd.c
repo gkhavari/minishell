5e447e00939c6d7a103d2ad4c6d6776f4a970265
Trung Nguyen <sendtotrungnguyen@outlook.com>
2025-12-01 18:20:09 +0100

    remove the malloc, to avoid free

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: thanh-ng <thanh-ng@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 20:29:49 by thanh-ng          #+#    #+#             */
-/*   Updated: 2025/12/01 18:15:43 by thanh-ng         ###   ########.fr       */
+/*   Updated: 2025/12/01 18:18:42 by thanh-ng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,9 @@
 */
 int	builtin_pwd(char **args, t_shell *shell)
 {
-	char	*cwd;
-
 	(void)args;
 	if (!shell->cwd)
 		return (1);
-	cwd = ft_strdup(shell->cwd); //this generates a non-freeable string, will be a problem
-	if (!cwd)
-	{
-		perror("minishell: pwd: malloc");
-		return (1);
-	}
-	ft_putendl_fd(cwd, 1);
-	free(cwd);
+	ft_putendl_fd(shell->cwd, 1);
 	return (0);
 }
