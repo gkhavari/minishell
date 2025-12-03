15d8b0b1edd4869c920b0cf770a41284fcd433c3
Trung Nguyen <sendtotrungnguyen@outlook.com>
2025-12-03 21:20:34 +0100

    add missing includes for unistd.h and sys/wait.h in includes.h

--- includes/includes.h
+++ includes/includes.h
@@ -17,6 +17,8 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <limits.h>
+# include <unistd.h>
+# include <sys/wait.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <errno.h>
