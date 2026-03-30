27840a6a6263e9022fb39e6c218bbd4b0828f483
Trung Nguyen <sendtotrungnguyen@outlook.com>
2026-03-30 23:19:48 +0200

    Restore ft_isspace prototype and implementation in libft

--- libft/libft.h
+++ libft/libft.h
@@ -38,6 +38,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int	ft_isspace(int c);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *str, size_t n);
